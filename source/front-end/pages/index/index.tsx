1fcc66a87ad3e2b29e02ecc4cfee412f65143300
Tertius Roach <tertius.roach@gmail.com>
2024-05-20 20:39:14 +0200

    Works on Windows 11.

--- source/front-end/pages/index/index.tsx
+++ source/front-end/pages/index/index.tsx
@@ -1,22 +1,22 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import maakBlou from '../../../api/toolbox/maakBlou';
-import maakGroen from '../../../api/toolbox/maakBlou';
+import maakGroen from '../../../api/toolbox/maakGroen';
 
 import './index.scss';
 
 const Body = () => {
   const eleName = 'my-ok';
-  const test = document.getElementById(eleName) as HTMLHeadElement | null;
 
-  // useEffect(() => {
-  //   if (test) {
-  //     maakBlou(test);
-  //     maakGroen(test);
-  //   } else {
-  //     console.error(`Cannot find Element ${eleName}`);
-  //   }
-  // }, []);
+  useEffect(() => {
+    const test = document.getElementById(eleName) as HTMLDivElement | null;
+    if (test) {
+      maakBlou(test);
+      maakGroen(test);
+    } else {
+      console.error(`Cannot find Element ${eleName}`);
+    }
+  }, [eleName]);
 
   return (
     <section id="my-ok" className={eleName}>
@@ -26,22 +26,3 @@ const Body = () => {
 };
 
 ReactDOM.render(<Body />, document.getElementById('root'));
-
-function Main(): void {
-  const eleName = `my-ok`;
-  // Sending Test
-  const test = document.getElementById(eleName) as HTMLHeadElement | null;
-  if (test) {
-    maakBlou(test);
-    maakGroen(test);
-  } else {
-    throw new Error(`Cannot find Element ${eleName}`);
-  }
-}
-try {
-  Main();
-} catch (err) {
-  if (err instanceof Error) {
-    alert(err.message);
-  }
-}
