b657e31741e254d81616fe13bf68235537313e36
Tertius Roach <tertius.roach@gmail.com>
2024-05-22 17:17:13 +0200

    CHANGES!

--- source/front-end/pages/index/A-body/react-body/react-body.tsx
+++ source/front-end/pages/index/A-body/react-body/react-body.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 const projectURI = 'https://raw.githubusercontent.com/TertiusRoach/development-portfolio_4.00/main';
+const CORE_CONCEPTS = [
+  {
+    image: `${projectURI}/source/front-end/pages/index/%7Econtent/png-files/react-concepts/components.png`,
+    title: 'Components',
+    description: 'The core UI building block - compose the user interface by combining multiple components.',
+  },
+  {
+    image: `${projectURI}/source/front-end/pages/index/%7Econtent/png-files/react-concepts/config.png`,
+    title: 'JSX',
+    description: 'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
+  },
+  {
+    image: `${projectURI}/source/front-end/pages/index/%7Econtent/png-files/react-concepts/jsx-ui.png`,
+    title: 'Props',
+    description: 'Make components configurable (and therefore reusable) by passing input data to them.',
+  },
+  {
+    image: `${projectURI}/source/front-end/pages/index/%7Econtent/png-files/react-concepts/state-mgmt.png`,
+    title: 'State',
+    description: 'React-managed data which, when changed, causes the component to re-render & the UI to update.',
+  },
+];
 
 function CoreConcept(props: string | any) {
-  let reactImages = ['components.png', 'config.png', 'jsx-ui.png', 'state-mgmt.png'];
-  let imageURI = `${projectURI}/source/front-end/pages/index/%7Econtent/png-files/react-concepts/${props.image}.png`;
-
+  // Props makes a function reusable
   return (
     <li>
-      <img src={`${imageURI}`} alt="..." />
+      <img src={`${props.image}`} alt="..." />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
     </li>
@@ -26,23 +46,15 @@ function reactBody(pageName: string | any, blockName: string | any) {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept title="Components" description="The core UI building block" image="components" />
-            <CoreConcept title="Props" description="The core UI building block" image="config" />
-            <CoreConcept title="{2}" description="The core UI building block" image="jsx-ui" />
-            <CoreConcept title="{3}" description="The core UI building block" image="state-mgmt" />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
     </>
   );
-}
-
-function randomizeItem(array: Array<string>) {
-  const items = array;
-  const max: number = items.length;
-  const randomize = Math.floor(Math.random() * (max + 1));
-
-  return items[randomize];
 }
 
 function Header() {
@@ -54,6 +66,14 @@ function Header() {
       <p>{randomizeItem(['Fundemental', 'Crucial', 'Core'])} React concepts you will need for almost any app you are going to build.</p>
     </header>
   );
+}
+
+function randomizeItem(array: Array<string>) {
+  const items = array;
+  const max: number = items.length;
+  const randomize = Math.floor(Math.random() * (max + 1));
+
+  return items[randomize];
 }
 
 export default reactBody;
