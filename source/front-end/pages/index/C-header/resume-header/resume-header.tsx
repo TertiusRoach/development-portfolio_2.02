3cf24461635c020fae0151fd988ceaefa24065f7
Tertius Roach <tertius.roach@gmail.com>
2024-05-26 19:28:02 +0200

    Icons added to laptop.

--- source/front-end/pages/index/C-header/resume-header/resume-header.tsx
+++ source/front-end/pages/index/C-header/resume-header/resume-header.tsx
@@ -2,20 +2,23 @@
 import React from 'react';
 
 function resumeHeader(pageName: string | any, blockName: string | any) {
+  const logoAdjacent: string = `https://raw.githubusercontent.com/TertiusRoach/development-portfolio_4.00/65b0fdc15d33bc59025893b17dea07b08dc3a52c/source/front-end/pages/index/%7Econtent/svg-files/signature-adjacent/primary-light.svg`;
+  const logoIcon: string = `https://raw.githubusercontent.com/TertiusRoach/development-portfolio_4.00/65b0fdc15d33bc59025893b17dea07b08dc3a52c/source/front-end/pages/index/%7Econtent/svg-files/signature-icon/primary-light.svg`;
   console.log('//--|🠊 resume-header.tsx loaded 🠈|--//');
   return (
     <>
       <aside>
-        <img
-          src="source/front-end/pages/resume/~content/svg-files/signature-adjacent/primary-light.svg"
-          alt="Résumé Logo"
-        />
+        <img src={logoAdjacent} alt="Résumé Logo" />
       </aside>
       <aside>
-        {/* <button>
-          <svg></svg>
-          <h6>Button</h6>
-        </button> */}
+        <menu>
+          <button>
+            <span>
+              <img src={logoIcon} alt="Résumé Logo" />
+              <h6>Button</h6>
+            </span>
+          </button>
+        </menu>
       </aside>
     </>
   );
