9fe2e3e2c542d759897f251bdce3475e6fe8acf8
Tertius Roach <tertius.roach@gmail.com>
2024-05-28 12:56:02 +0200

    Home section built.

--- source/front-end/pages/index/G-main/resume-main/resume-main.tsx
+++ source/front-end/pages/index/G-main/resume-main/resume-main.tsx
@@ -4,6 +4,10 @@ import React from 'react';
 function resumeMain(pageName: string | any, blockName: string | any) {
   const page = pageName;
   const block = blockName;
+
+  const jobTitle: Array<string> = ['Multimedia Programmer'];
+  const profilePicture: string = `https://raw.githubusercontent.com/TertiusRoach/development-portfolio_4.00/main/source/front-end/pages/index/%7Econtent/png-files/profile-picture.png`;
+
   console.log('//--|🠊 resume-main.tsx loaded 🠈|--//');
   return (
     <>
@@ -11,20 +15,58 @@ function resumeMain(pageName: string | any, blockName: string | any) {
         <section className="main-home">
           <div className="margin-main">
             <aside id="cover-letter">
-              <h1>Title</h1>
-              <p>Description</p>
-              <menu>My Career + Contact Me</menu>
+              <h1 className="job-title" data-text={jobTitle[0]}>
+                {jobTitle[0]}
+              </h1>
+              <menu className="home-buttons">
+                <button className="my-career">
+                  <a href="">
+                    <h6>My Career</h6>
+                  </a>
+                </button>
+                <button className="contact-me">
+                  <a href="">
+                    <h6>Contact Me</h6>
+                  </a>
+                </button>
+              </menu>
+
+              <p>
+                As a full-stack developer, my focus is on building
+                websites, animations, and applications designed to
+                captivate users from the first click. By blending
+                creativity with technical expertise, I aim to create
+                standout projects that strive to make our lives easier.
+                There's a genuine passion within me to experiment with new
+                tools and stay updated with multimedia technologies,
+                allowing me to bring fresh and innovative solutions to each
+                project. I'm eager to collaborate with fellow creatives,
+                employers, and enthusiasts to turn concepts into impactful,
+                enduring applications. While lacking a traditional CS
+                degree, my unique perspective undoubtedly adds value. I
+                thrive in environments conducive to exploring new tools
+                with clear goals, and my passion lies in partnering with
+                visionary creatives to transform concepts into digital
+                masterpieces. Through hands-on experience and a continuous
+                learning journey, my arsenal of skills can deliver
+                exceptional results to anyone seeking my services, whether
+                permanent or contract-based.
+              </p>
+
+              <div className="mid-ground"></div>
             </aside>
             <aside id="profile-picture">
-              <img src="" alt="Profile Picture" />
+              <img src={profilePicture} alt="Profile Picture" />
             </aside>
           </div>
         </section>
+
         <section className="main-skills">
           <div className="margin-main">
             <h1>Skills</h1>
           </div>
         </section>
+
         <section className="main-contact">
           <div className="margin-main">
             <h1>Contact</h1>
